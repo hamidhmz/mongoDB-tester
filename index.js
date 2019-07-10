e11dd5cb89b76d5114c735a2324b3a6dad7f87b5
HamidReza Nasrollahi <nasrollahy.hamidreza@gmail.com>
2019-07-10 09:36:26 +0430

    s

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 
 
 app.get("/mongo-client/:count",async (req,res)=>{
-    res.send("hi"+req.params.count);
+    res.send("hi "+req.params.count);
 });
 app.get("/mongoose/:count",async (req,res)=>{
-    res.send("hi"+req.params.count);
+    res.send("hi "+req.params.count);
 });
 
 app.listen(process.env.PORT,()=>{
